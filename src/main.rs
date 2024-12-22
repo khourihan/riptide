e48aa30329362f79b82acadbe4da955ed24449b5
Killian Hourihan <kllnhrhn@gmail.com>
2024-12-22 17:52:14 +0800

    Add circle drawing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use draw::DrawState;
+use glam::{Vec2, Vec4};
 
 mod draw;
 
 fn main() {
     let mut state = DrawState::new("output/test.mp4", 1280, 720, 24);
 
-    for _ in 0..256 {
-        state.draw_next();
+    let mut i = 0.0;
+    while i < 1.0 {
+        i += 0.01;
+
+        state.circle(Vec2::new(i, 0.5), 5.0, Vec4::new(0.1, 0.4, 0.8, 0.75));
+        state.next();
     }
 
     state.finish();
